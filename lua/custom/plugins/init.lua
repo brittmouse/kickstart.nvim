2fdda0ee64fa2a482903a026501e9a3f2d72a1c6
brittmouse <bstancoff@proton.me>
2025-07-01 18:46:29 -0400

    Customize with Snacks, leap, and vim-typst

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,33 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'rose-pine/neovim',
+    name = 'rosepine',
+    config = function()
+      vim.cmd 'colorscheme rose-pine'
+    end,
+  },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    lazy = false,
+    config = function()
+      require('oil').setup {
+        vim.keymap.set('n', '-', '<cmd>Oil<cr>', { desc = 'Open parent dir' }),
+      }
+    end,
+  },
+  {
+    'ggandor/leap.nvim',
+    dependencies = { 'tpope/vim-repeat' },
+    lazy = false,
+    config = function()
+      require('leap').set_default_mappings()
+    end,
+  },
+}
